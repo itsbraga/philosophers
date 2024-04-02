21542d5b6a66cd77058a23147d82dad51c6ec3ab
Anna BRAGA <annabrag@bess-f4r4s11.clusters.42paris.fr>
2024-04-02 15:31:07 +0200

    restructured

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,8 +6,21 @@
 /*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 16:37:14 by annabrag          #+#    #+#             */
-/*   Updated: 2024/04/01 16:37:18 by annabrag         ###   ########.fr       */
+/*   Updated: 2024/04/02 15:21:39 by annabrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	pthread_t	thread;
+	long long	time_to_eat;
+	long long	time_to_sleep;
+	long long	time_to_die;
+}				t_philo;
